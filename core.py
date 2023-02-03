ea83e5b1dadf6d263f9a4dea568ca77f3868167f
Eric Dasse <christ.gououi@gmail.com>
2023-02-03 23:32:58 +0100

    Add `DogAPI` class
    This class will pose as an intermediate between the dog API and our code

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 
 
@@ -11,3 +12,17 @@ class RequestType(Enum):
     POST = "POST"
     PATCH = "PATCH"
     DELETE = "DELETE"
+
+
+class DogAPI:
+    def __init__(self):
+        """
+        Function to initialize the Dog API class
+        """
+
+        api_key = os.environ.get("API_KEY")
+        self.headers = {
+            "Content-Type": "application/json",
+            "x-api-key": api_key,
+        }
+        self.base_url = "https://api.thedogapi.com/v1"
